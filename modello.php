164875421e2199e5d87c4c741327bd83414ff3cc
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-10-04 22:06:49 +0200

    OOP PHP

---
+++ modello.php
@@ -0,0 +1,11 @@
+<?php
+      echo "<div class='card d-flex flex-wrap'>
+        <div class='card-body'>
+          <h5 class='card-title'>$identificatore</h5>
+          <p class='card-text'>$modelloProdotto</p>
+          <p class='card-text'>$prezzo</p>
+          <a href='#' class='btn btn-primary'>Go somewhere</a>
+        </div>
+      </div>";
+
+
